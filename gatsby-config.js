f390ba1983c19952a0256eccdf84cd0309c3247d
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2022-01-04 16:34:43 +0000

    Remove Google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,4 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Personal Blog Website",
   },
-  plugins: [
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
-  ],
 };
