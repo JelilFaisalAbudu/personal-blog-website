0d4b32e220035b2812a12f2515ed363624dc5eb6
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2022-01-31 20:02:22 +0000

    Use the StaticImage component to add an external image to the site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // markup
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>
-        Hi there!I 'm the proud creator of this site, which I built with Gatsby.
+        Hi there! I 'm the proud creator of this site, which I built with Gatsby.
       </p>
+      <StaticImage
+        alt="Zen, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   )
 }
