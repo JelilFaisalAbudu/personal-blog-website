a4e3b6184ab28522451090c6a8a86a5d9a1df26f
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2022-02-01 22:44:30 +0000

    Add the blog page component

---
+++ src/pages/blog.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const BlogPage = () => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <p>
+        All the cool posts will come your way soon
+      </p>
+    </Layout>
+  );
+}
+
+export default BlogPage;
